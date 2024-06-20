0008e11dbce3c578b0395fe71faa62cc158af616
jazr4 <jazminaramos4@hotmail.com>
2024-06-20 15:47:38 -0300

    pe2 asoma taiwlind

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,33 +2,29 @@ import { CartWidget } from '../CartWidget/CartWidget';
 import { Link } from "react-router-dom";
 
 export const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid justify-content-center">
-                <div className="nav" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
+    return (  
+        <div className="flex-auto container mx-auto grid gap-x-0 grid-cols-6">
                         <Link to={"/"}>
-                            <button className="btn btn-outline-dark mx-2">Home</button>
+                <button className="px-6 py-2 bg-red-300 hover:bg-red-400">Home</button>
                         </Link>
                         <Link to={"/category/lapiceras"}>
-                            <button className="btn btn-outline-dark mx-2">Lapiceras</button>
+                <button className="px-6 py-2 bg-orange-300 hover:bg-orange-400">Lapiceras</button>
                         </Link>
                         <Link to={"/category/cuadernos"}>
-                            <button className="btn btn-outline-dark mx-2">Cuadernos</button>
+                <button className=" px-6 py-2 bg-yellow-300 hover:bg-yellow-500">Cuadernos</button>
                         </Link>
                         <Link to={"/category/resaltadores"}>
-                            <button className="btn btn-outline-dark mx-2">Resaltadores</button>
+                <button className="px-6 py-2 bg-lime-300 hover:bg-lime-500">Resaltadores</button>
                         </Link>
                         <Link to={"/category/lapices"}>
-                            <button className="btn btn-outline-dark mx-2">Lapices</button>
+                <button className="px-6 py-2 bg-blue-300 hover:bg-blue-400">Lapices</button>
                         </Link>
-                    </div>
-                </div>
-            </div>
-            <div className=" d-flex ">
+                    
+           
+            <div>
                 <CartWidget />
-                <p className="badge border border-light rounded-circle bg-danger p-1">2</p>
+                <p className="">2</p>
             </div>
-        </nav>
+    </div>
     );
 };
